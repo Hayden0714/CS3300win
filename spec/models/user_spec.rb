3bd5f7c54989a436efe6d6592e6c444229f95ef6
Hayden Olmstead <hayden.olmstead@gmail.com>
2021-11-18 21:36:54 +0000

    all the tests are passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,6 +8,32 @@ RSpec.describe User, type: :model do
     end
   end
 
-  
+  context 'ensures presence of a username' do
+    it 'ensures the user entered a username' do
+      user = User.new(firstname: 'first', lastname: 'last', username: '', password: '123', email: 'sample@example.com').save
+      expect(user).to eq(false)
+    end
+  end
+
+  context 'ensures the presence of a first name'do
+    it 'ensures the user entered a first name' do
+      user = User.new(firstname: '', lastname: 'last', username: '', password: '123', email: 'sample@example.com').save
+      expect(user).to eq(false)
+    end
+  end
+
+  context 'ensures the presence of a last name'do
+    it 'ensures the user entered a last name' do
+      user = User.new(firstname: 'first', lastname: '', username: '', password: '123', email: 'sample@example.com').save
+      expect(user).to eq(false)
+    end
+  end
+
+  context 'ensures the presence of a password'do
+    it 'ensures the user entered a password' do
+      user = User.new(firstname: 'first', lastname: 'last', username: 'username', password: '', email: 'sample@example.com').save
+      expect(user).to eq(false)
+    end
+  end
 
 end
