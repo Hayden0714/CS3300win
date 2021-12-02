ed888c8ac1b7c21569c52d4888deede7e39b9530
Hayden Olmstead <hayden.olmstead@gmail.com>
2021-12-02 08:01:45 +0000

    just got the signup page working

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :password, presence:true
-  validates :firstname, presence: true
-  validates :lastname, presence: true
+  
   validates :email, presence: true
   validates :username, uniqueness: {case_sensitive: false}, presence: true
 end
