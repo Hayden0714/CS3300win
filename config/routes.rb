296784746002de052c150d86985c285192bde17c
hayden <hayden.olmstead@gmail.com>
2021-12-07 05:00:07 +0000

    some of the front end is working

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "projects#index"
   resources :projects
-
-  devise_scope :user do
-    get "/home", :to => "devise/sessions#new", :as => :home 
-    post "/users/sign_up", :to => "devise/sessions#create", :as => :sign_up
-  end
 end
 
