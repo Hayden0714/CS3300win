04095a58b46d3a7279521f6798a6e2105de5db54
Hayden Olmstead <hayden.olmstead@gmail.com>
2021-12-08 02:27:59 +0000

    heroku test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root "projects#index"
-  resources :projects
-
-  devise_scope :user do
-    get "/home", :to => "devise/sessions#new", :as => :home 
-    post "/users/sign_up", :to => "devise/sessions#create", :as => :sign_up
-  end
+  resources :projects 
 end
 
